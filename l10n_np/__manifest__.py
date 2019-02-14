1912a19855b578814af9925846352b2e15f0490d
Ashim Bajracharya <ashimbazracharya@gmail.com>
2019-02-14 19:06:25 +0545

    [ADD] l10n_np: add module

---
+++ l10n_np/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Nepali - Accounting',
+    'author': 'Ygen Software',
+    'website': 'http://ygen.io',
+    'category': 'Localization',
+    'description': """
+Nepal accounting chart and localization.
+=======================================================
+
+    """,
+    'depends': ['base', 'account'],
+    'data': [
+             # 'data/account_data.xml',
+             # 'data/l10n_ae_chart_data.xml',
+             # 'data/account_chart_template_data.yml',
+    ],
+}
