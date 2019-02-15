cb6268372f36681ab9dad0c37e6bb0fd6ede82f3
susankc <susan.kc13@gmail.com>
2019-02-15 16:28:56 +0545

    [IMP] l10n_np: inherits res.company and add district and state.

--- l10n_np/models/res_company.py
+++ l10n_np/models/res_company.py
@@ -4,7 +4,10 @@
 from odoo import fields, models
 
 
-class CountryState(models.Model):
+class ResCountry(models.Model):
     _inherit = 'res.company'
 
     vat = fields.Char(related='partner_id.vat', string="PAN")
+    district_id = fields.Many2one('res.country.district', string="District")
+    l10n_np_state_ids = fields.Many2one(related='district_id.state_ids', string="States")
+    # state_ids = fields.Many2one(related='country_id.state_ids', string="States")
