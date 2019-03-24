b2d7acf4c9aa8701201f40aa226d17ccc4040329
Ashim Bajracharya <ashimbazracharya@gmail.com>
2019-03-24 17:32:50 +0545

    [REF] partner_company_chamber: refactor code

--- partner_company_chamber/models/account_invoice.py
+++ partner_company_chamber/models/account_invoice.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
     
-    related_chamber = fields.Char(related='partner_id.chamber')
+    related_chamber = fields.Char(related='partner_id.chamber', store=True)
--- partner_company_chamber/models/sale.py
+++ partner_company_chamber/models/sale.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
     
-    related_chamber = fields.Char(related='partner_id.chamber')
+    related_chamber = fields.Char(related='partner_id.chamber', store=True)
