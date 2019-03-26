4bcc49719dec81311d6e45a4b9bbb0e4d7041235
Ashim Bajracharya <ashimbazracharya@gmail.com>
2019-03-26 18:55:24 +0545

    [REF] partner_company_chamber: refactor fields description

--- partner_company_chamber/models/company.py
+++ partner_company_chamber/models/company.py
@@ -5,7 +5,7 @@ class ResCompany(models.Model):
     """Add chamber number field in company model"""
     _inherit = "res.company"
 
-    chamber = fields.Char('Chamber Number')
+    chamber = fields.Char('Chamber No.')
 
     _sql_constraints = [
         ('unique_chamber', 'unique (chamber)', 'This chamber no. is already registered in the system !'),
