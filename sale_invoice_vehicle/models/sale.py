934dbf51c57547e73f5ce432f9b4ee3c26fccfe4
Ashim Bajracharya <ashimbazracharya@gmail.com>
2019-03-26 18:56:27 +0545

    [REF] sale_invoice_vehicle: override method "prepare_invoice"

--- sale_invoice_vehicle/models/sale.py
+++ sale_invoice_vehicle/models/sale.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     tanker_vehicle_number = fields.Char('Vehicle No.')
+
+    # Override method to pass tanker vehicle number in the dictionary 
+    # before creating invoice object of the respective sale order line
+    # if the invoice advance payment is of type delivered or deduct down
+    # payments.
+    def _prepare_invoice(self):
+	    invoice_vals = super(SaleOrder, self)._prepare_invoice()
+	    invoice_vals.update({
+	        'tanker_vehicle_number': self.tanker_vehicle_number,
+	    })
+	    return invoice_vals
