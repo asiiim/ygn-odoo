6ea21ca807676c25f9f4fe719256e1caeb2d5fcc
Ashim Bajracharya <ashimbazracharya@gmail.com>
2019-04-13 14:14:28 +0545

    [storage, station] fix fields and issues

--- storage/models/category.py
+++ storage/models/category.py
@@ -9,8 +9,6 @@ class StorageCategory(models.Model):
     name = fields.Char("Name")
     
     sequence = fields.Integer(help="Gives the sequence order when displaying a list of categories.")
-
-    diptest_id = fields.Many2one('storage.diptest', string="Dip Test Formula")
     
     formula = fields.Text(string="Volume Storage Formula",
         help="Formula to calculate the volume of the storage.\n\n"
@@ -19,7 +17,7 @@ class StorageCategory(models.Model):
              "\n%(breadth)f: the breadth of the storage"
              "\n%(height)f: the height of the storage"
              "\n%(diameter)f: the diameter of the storage"
-             "\n%(PI)f: value of the Pi",
+             "\n%(pi)f: value of the Pi",
         default='%(length)f*%(breadth)f*%(height)f*%(diameter)f')
 
     diptest_formula = fields.Text(string="Dip Test Formula",
@@ -29,7 +27,8 @@ class StorageCategory(models.Model):
              "\n%(breadth)f: the breadth of the storage"
              "\n%(height)f: the height of the storage"
              "\n%(diameter)f: the diameter of the storage"
-             "\n%(PI)f: value of the Pi",
-        default='%(length)f*%(breadth)f*%(height)f*%(diameter)f')
+             "\n%(dip)f: the dip value"
+             "\n%(pi)f: value of the Pi",
+        default='%(length)f*%(breadth)f*%(height)f*%(dip)f')
 
 
