65e6addf1b3daadc8271063fac572f61306adc2a
Bishal Pun <bishalpun2013@gmail.com>
2019-04-14 21:39:30 +0545

    [ADD] l10n_np_resource for working times in Nepal
    This module adds 3 possible working times for Nepal.
    One is for government and other 2 for private businesses.
    These working times are general. Break times have to be
    adjusted as per the need of an enterprise.

---
+++ l10n_np_resource/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Nepali - HR',
+    'version': '1.0',
+    'author': 'Ygen Software Pvt. Ltd.',
+    'website': 'http://ygen.io',
+    'category': 'Localization',
+    'description': """
+This is the base module to manage the hr for Nepal in Odoo.
+==============================================================================
+
+    """,
+    'depends': ['hr'],
+    'data': [
+        'data/resource_data.xml',
+    ],
+}
