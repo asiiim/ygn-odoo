4d43b4fca87baa9613d0fca1bc334af356b29a8e
Bishal Pun <bishalpun2013@gmail.com>
2019-04-24 13:16:59 +0545

    [IMP] l10n_np: print copy of original and copy no
    Before this commit, odoo allows invoices to be printed any number
    of times.
    
    After this commit, first print is original one and all other
    prints of a specific invoice is copy of original. Copy of
    Original term is printed in such invoice and number of copies
    printed till is also mentioned in the print. This commit also
    logs changes in the copy count inside chatter.

--- l10n_np/models/account_invoice.py
+++ l10n_np/models/account_invoice.py
@@ -14,3 +14,15 @@ class AccountInvoice(models.Model):
             invoice.amount_total_words = invoice.currency_id.amount_to_text(invoice.amount_total)
 
     amount_total_words = fields.Char("Total (In Words)", compute="_compute_amount_total_words")
+    printed_copy_count = fields.Integer(string="Copy no.", readonly=True, default=0, copy=False,
+        track_visibility='onchange', help="It indicates the number of times a copy of invoice has been printed.")
+    
+    # Override
+    @api.multi
+    def invoice_print(self):
+        """ Print the copy of original invoice and increment the printed copy count
+        """
+        self.ensure_one()
+        if self.sent:
+            self.printed_copy_count += 1
+        return super(AccountInvoice, self).invoice_print()
