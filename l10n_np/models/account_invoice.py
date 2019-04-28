ae0b74a8de1ac6cd2f081899ad6588ff31345bd4
Bishal Pun <bishalpun2013@gmail.com>
2019-04-28 10:52:19 +0545

    [IMP] l10n_np: store print by and printed on values
    This commit adds print_uid and print_datetime so that the
    original print datetime and uid could be tracked.

--- l10n_np/models/account_invoice.py
+++ l10n_np/models/account_invoice.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Ygen. See LICENSE file for full copyright and licensing details.
 
+import datetime
+
 from odoo import api, fields, models, _
 
 
@@ -16,7 +18,11 @@ class AccountInvoice(models.Model):
     amount_total_words = fields.Char("Total (In Words)", compute="_compute_amount_total_words")
     printed_copy_count = fields.Integer(string="Copy no.", readonly=True, default=0, copy=False,
         track_visibility='onchange', help="It indicates the number of times a copy of invoice has been printed.")
-    
+    print_datetime = fields.Datetime(string='Printed on', readonly=True, copy=False, track_visibility='onchange',
+        help="It is the datetime on which original invoice is printed.")
+    print_uid = fields.Many2one('res.users', string='Printed by', readonly=True, copy=False, track_visibility='onchange',
+        help="It is the user who prints the original invoice.")
+
     # Override
     @api.multi
     def invoice_print(self):
@@ -25,4 +31,6 @@ class AccountInvoice(models.Model):
         self.ensure_one()
         if self.sent:
             self.printed_copy_count += 1
+        else:
+            self.write({'print_datetime':str(datetime.datetime.utcnow()), 'print_uid': self._uid})
         return super(AccountInvoice, self).invoice_print()
