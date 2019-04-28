b497d50834e703317fd82c1384a51bd85ff29389
Bishal Pun <bishalpun2013@gmail.com>
2019-04-28 10:56:28 +0545

    [ADD] l10n_np_sales: compute export, taxable, non-taxable amounts
    This module adds total export amount, total taxable sales, total
    non-taxable sales (tax-exempted sales) required for IRD.

---
+++ l10n_np_sales/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Ygen. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Nepal - Sales Book',
+    'version': '1.0',
+    'author': 'Ygen Software Pvt. Ltd.',
+    'website': 'http://ygen.io',
+    'category': 'Localization',
+    'description': """
+This module adds sales book for Nepal.
+==============================================================================
+
+    """,
+    "installable": True,
+    'depends': ['l10n_np'],
+    'data': [
+    ],
+}
