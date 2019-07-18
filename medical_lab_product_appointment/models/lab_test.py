16f1d4abf2f0977ce95bf0ed772a1b795a79d87f
Ashim Bajracharya <ashimbazracharya@gmail.com>
2019-07-18 12:19:42 +0545

    [REF] improve issue of accounting

--- medical_lab_product_appointment/models/lab_test.py
+++ medical_lab_product_appointment/models/lab_test.py
@@ -22,14 +22,23 @@ class Appointment(models.Model):
     def create_invoice(self):
         invoice_obj = self.env["account.invoice"]
         invoice_line_obj = self.env["account.invoice.line"]
+
+        self.ensure_one()
+        journal_id = self.env['account.invoice'].default_get(['journal_id'])['journal_id']
+        if not journal_id:
+            raise UserError(_('Please define an accounting sales journal for this company.'))
+        
         for lab in self:
             lab.write({'state': 'to_invoice'})
             if lab.patient_id:
                 curr_invoice = {
+                    'name': lab.patient_id.patient.name or '',
                     'partner_id': lab.patient_id.patient.id,
                     'account_id': lab.patient_id.patient.property_account_receivable_id.id,
+                    'journal_id': journal_id,
                     'state': 'draft',
                     'type': 'out_invoice',
+                    'comment': self.comment,
                     'date_invoice': datetime.datetime.now(),
                     'origin': "Lab Test# : " + lab.name,
                     'target': 'new',
@@ -41,16 +50,21 @@ class Appointment(models.Model):
                 inv_id = inv_ids.id
 
                 if inv_ids:
-                    journal = self.env['account.journal'].search([('type', '=', 'sale')], limit=1)
-                    prd_account_id = journal.default_credit_account_id.id
                     if lab.appointment_lines:
                         for line in lab.appointment_lines:
+
+                            line.ensure_one()
+                            account = line.lab_test.product_id.property_account_income_id or line.lab_test.product_id.categ_id.property_account_income_categ_id
+                            if not account:
+                                raise UserError(_('Please define income account for this product: "%s" (id:%d) - or for its category: "%s".') %
+                                    (line.lab_test.product_id.name, line.lab_test.product_id.id, line.lab_test.product_id.categ_id.name))
+
                             curr_invoice_line = {
                                 'name': line.lab_test.product_id.name,
                                 'price_unit': line.lab_test.product_id.list_price or 0,
                                 'invoice_id': inv_id,
-                                'product_id': line.lab_test.product_id.id,
-                                'account_id': prd_account_id,
+                                'product_id': line.lab_test.product_id.id or False,
+                                'account_id': account.id,
                             }
                             invoice_line_obj.create(curr_invoice_line)
 
