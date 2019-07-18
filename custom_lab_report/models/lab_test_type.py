dc883a82c59e68fe45a4c40baa8c1642f2207600
susankc <susan.kc13@gmail.com>
2019-07-18 16:24:35 +0545

    [IMP]custom_lab_report: Add field in lab.test and override action_request of lab.appointment

--- custom_lab_report/models/lab_test_type.py
+++ custom_lab_report/models/lab_test_type.py
@@ -2,12 +2,41 @@
 
 from odoo import models, fields, api
 import logging
+import datetime
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
 
-class ProductTemplate(models.Model):
+class CustomLabRequest(models.Model):
     _inherit = 'lab.request'
     
     text_after_result = fields.Html('Text After Result')
     text_before_result = fields.Html('Text Before Result')
+
+class CustomLabTest(models.Model):
+    _inherit = 'lab.test'
+    
+    text_after_result = fields.Html('Text After Result')
+    text_before_result = fields.Html('Text Before Result')
+
+class CustomLabAppointment(models.Model):
+    _inherit = 'lab.appointment'
+    
+    @api.multi
+    def action_request(self):
+        if self.appointment_lines:
+            for line in self.appointment_lines:
+                data = self.env['lab.test'].search([('lab_test', '=', line.lab_test.lab_test)])
+                self.env['lab.request'].create({'lab_request_id': self.name,
+                                                'app_id': self.id,
+                                                'lab_requestor': self.patient_id.id,
+                                                'lab_requesting_date': self.appointment_date,
+                                                'test_request': line.lab_test.id,
+                                                'text_after_result': line.lab_test.text_after_result,
+                                                'text_before_result': line.lab_test.text_before_result,
+                                                'request_line': [(6, 0, [x.id for x in data.test_lines])],
+                                                })
+            self.state = 'request_lab'
+        else:
+            raise UserError(_('Please Select Lab Test.'))
