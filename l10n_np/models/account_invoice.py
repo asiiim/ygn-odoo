d65189eb083f536232105c169c7a59335c64a888
Bishal Pun <bishalpun2013@gmail.com>
2019-09-13 19:28:50 +0545

    [IMP] add transaction and new copy of original policy
    Previous Policy for Copy of Original:
    The first print is original one and any prints after the
    first one there needs to be "Copy of Original" and copy
    count written in the invoice.
    
    Current Policy for Copy of Original:
    The first print must print "Tax Invoice" and the second
    print onward must print "Invoice". The third print
    onward must include "Copy of Original" and copy count.

--- l10n_np/models/account_invoice.py
+++ l10n_np/models/account_invoice.py
@@ -22,6 +22,18 @@ class AccountInvoice(models.Model):
         help="It is the datetime on which original invoice is printed.")
     print_uid = fields.Many2one('res.users', string='Printed by', readonly=True, copy=False, track_visibility='onchange',
         help="It is the user who prints the original invoice.")
+    date_transaction = fields.Datetime(string='Transaction Date', readonly=True, copy=False, track_visibility='onchange',
+        help="It is the datetime on which transaction occurs.")
+    tax_invoice_printed = fields.Boolean(readonly=True, default=False, copy=False,
+        help="It indicates that the tax invoice has been printed.")   
+    
+    # Override
+    @api.multi
+    def action_invoice_open(self):
+        open_invoices = super(AccountInvoice, self).action_invoice_open()
+        # set transaction date
+        self.write({"date_transaction": fields.Datetime().now()})
+        return open_invoices
 
     # Override
     @api.multi
@@ -29,6 +41,20 @@ class AccountInvoice(models.Model):
         """ Print the copy of original invoice and increment the printed copy count
         """
         self.ensure_one()
+        # Check if tax invoice has been printed
+        # If it has been then increase the copy count
+        # So, the first time tax invoice will be printed
+        # And invoice will be printed. After that any prints 
+        # will be counted as copy of original.
+        # if self.sent and self.tax_invoice_printed:
+        #     self.printed_copy_count += 1
+        # else:
+        #     self.write({'print_datetime':str(datetime.datetime.utcnow()), 'print_uid': self._uid})
+        # if self.sent:
+        #     self.tax_invoice_printed = True
+
+        # This implementation makes Copy of Original as soon as 
+        # when the first print is done
         if self.sent:
             self.printed_copy_count += 1
         else:
