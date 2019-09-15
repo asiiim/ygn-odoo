e10aa93b6d96ff7206fd6c9647547ba23310a73a
Bishal Pun <bishalpun2013@gmail.com>
2019-09-15 12:59:00 +0545

    [IMP] fix total discount logic

--- account_invoice_total_discount/models/account_invoice.py
+++ account_invoice_total_discount/models/account_invoice.py
@@ -14,7 +14,7 @@ class AccountInvoice(models.Model):
     @api.depends('invoice_line_ids.quantity','invoice_line_ids.price_unit','invoice_line_ids.discount','invoice_line_ids.price_subtotal')
     def _compute_discount_total(self):
         for inv in self:
-            inv.amount_discount_total = sum(line.price_subtotal / (1 - (line.discount or 0.0) / 100) for line in inv.invoice_line_ids)
+            inv.amount_discount_total = sum((line.price_subtotal / (1 - (line.discount or 0.0) / 100)) - line.price_subtotal for line in inv.invoice_line_ids)
             # total_discount_amount = 0.0 
             # for line in invoice.invoice_line_ids:
             #     discount_amount = (line.quantity * line.price_unit) - line.price_subtotal
