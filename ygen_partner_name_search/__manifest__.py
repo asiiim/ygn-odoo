ba581484df116fed077130a4876bc91ca4a2c631
Ashim Bajracharya <ashimbazracharya@gmail.com>
2019-10-16 07:34:24 +0545

    [ADD] name search for contacts

---
+++ ygen_partner_name_search/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Ygen. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Contact Name Search",
+
+    'summary': """
+        Search or create contacts from many2field in other views.""",
+
+    'author': "Ygen Software Pvt. Ltd.",
+    'website': "https://ygen.io",
+
+    # Categories can be used to filter modules in modules listing
+    # for the full list
+    'category': 'Search',
+    'license': 'OPL-1',
+    'version': '11.0.1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # price
+    'price': 200.00,
+    'currency': 'EUR',
+}
