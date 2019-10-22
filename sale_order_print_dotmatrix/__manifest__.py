ae3f94d1b1b8b272086686fd7d2f8899cc3c8038
Ashim Bajracharya <ashimbazracharya@gmail.com>
2019-10-22 17:47:13 +0545

    [ADD] order ticket print in dot matrix

---
+++ sale_order_print_dotmatrix/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Ygen. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Order Ticket Dot Matrix Print',
+    'summary': 'Order has been designed specifically for dot matrix printers.',
+    'version': '11.0.1.0.0',
+    'author': 'Ygen Software Pvt. Ltd.',
+    'category': 'Accounting',
+    'license': 'OPL-1',
+    'complexity': 'normal',
+    'website': "https://ygen.io",
+    'depends': ['base', 'sale', 'ygen_win32_print', 'sale_workflow_cakeshop'],
+    'data': [
+        'views/report_order_ticket.xml',
+        'views/order_ticket_report.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+}
