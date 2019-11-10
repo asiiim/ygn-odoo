593e0472e0174bd55aff4c7897ca9354e65fa9e4
Bishal Pun <bishalpun2013@gmail.com>
2019-11-10 17:20:15 +0545

    [ADD] add zoom icon to zoom product template's image

---
+++ product_template_zoom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Ygen. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Product Tempate Image Zoom',
+    'version': '11.0.1.0.0',
+    'author': "Bishal Pun, "
+              "Ygen Software Pvt Ltd (Ygen)",
+    "website": "https://ygen.io",
+    'category': 'Sales',
+    'summary': "User can zoom product template's image from Kanban view.",
+    'depends': ['product'],
+    "data": [
+        # 'data/stock_data.xml',
+        'views/product_image_view.xml',
+    ],
+    'license': 'OPL-1',
+    'installable': True,
+    'application': False,
+}
