a43875229b40d061eecdc9566ee696d3025459a8
Bishal Pun <bishalpun2013@gmail.com>
2020-02-26 04:21:12 +0545

    [IMP] fix method decorators

--- stock_picking_analysis/models/stock.py
+++ stock_picking_analysis/models/stock.py
@@ -23,34 +23,33 @@ class Picking(models.Model):
 
     @api.multi
     def button_validate(self):
-        for rec in self:
+        self.ensure_one()
+        # Call super to get done date
+        validate = super(Picking, self).button_validate()
 
-            # Call super to get done date
-            validate = super(Picking, self).button_validate()
+        # Calculate delivery delay and delivery process time
+        date_done = fields.Datetime.from_string(self.date_done)
+        delivery_created = fields.Datetime.from_string(self.create_date)
+        delivery_scheduled = fields.Datetime.from_string(self.scheduled_date)
 
-            # Calculate delivery delay and delivery process time
-            date_done = fields.Datetime.from_string(rec.date_done)
-            delivery_created = fields.Datetime.from_string(rec.create_date)
-            delivery_scheduled = fields.Datetime.from_string(rec.scheduled_date)
+        delv_delay = date_done - delivery_scheduled
+        delv_processed = date_done - delivery_created
 
-            delv_delay = date_done - delivery_scheduled
-            delv_processed = date_done - delivery_created
+        delv_delay_hrs = 0.0
+        delv_processed_hrs = 0.0
 
+        # Get float values of the time intervals into hrs and minutes
+        if delv_delay.days >= 0:
+            delv_delay_hrs = delv_delay.seconds / 3600
+        else:
             delv_delay_hrs = 0.0
-            delv_processed_hrs = 0.0
 
-            # Get float values of the time intervals into hrs and minutes
-            if delv_delay.days >= 0:
-                delv_delay_hrs = delv_delay.seconds / 3600
-            else:
-                delv_delay_hrs = 0.0
+        delv_processed_hrs = delv_processed.seconds / 3600
 
-            delv_processed_hrs = delv_processed.seconds / 3600
+        # Write Delivery Date, Delay and Process time
+        self.write({
+            'delivery_delay': delv_delay_hrs,
+            'delivery_process_time': delv_processed_hrs
+        })
 
-            # Write Delivery Date, Delay and Process time
-            rec.write({
-                'delivery_delay': delv_delay_hrs,
-                'delivery_process_time': delv_processed_hrs
-            })
-
-            return validate
+        return validate
